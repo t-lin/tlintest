087b40815734de89cfa5a013fbe0f8c2569d0803
Thomas Lin <thomas.lin@spaceryde.com>
2021-12-06 15:20:15 -0500

    Add first basic test w/ google test (producer/consumer w/ channel size 1)

--- channel_test.cpp
+++ channel_test.cpp
@@ -1,9 +1,15 @@
+// C++ libs
 #include <iostream>
 #include <thread>
 
+// C libs
 #include <stdint.h>
 
+// Project libs
 #include "channel.hpp"
+
+// Google test, duh
+#include "gtest/gtest.h"
 
 using namespace std;
 
@@ -16,7 +22,7 @@ void producer(int n, Channel<uint8_t>& chan) {
 }
 
 
-int main() {
+bool TestChanSize1() {
   int numItems = 200;
   Channel<uint8_t> byteChan(1);
 
@@ -27,7 +33,7 @@ int main() {
   for (int i = 0; i < numItems; i++) {
     if (!byteChan.Get(val)) {
       cout << "Uh oh, Get() failed!" << endl;
-      break;
+      return false;
     }
 
     cout << "Got " << (int)val << endl;
@@ -36,5 +42,9 @@ int main() {
   prod.join();
   cout << "Bye!" << endl;
 
-  return 0;
+  return true;
+}
+
+TEST(ChannelTest, ChanSize1) {
+  EXPECT_TRUE(TestChanSize1());
 }
